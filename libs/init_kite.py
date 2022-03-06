d06859f734bdcaf2d7bf76ad87b5ff689425264f
ronscoder <rons.ubm@gmail.com>
2022-03-06 16:48:27 +0530

    Bug fix

--- libs/init_kite.py
+++ libs/init_kite.py
@@ -7,7 +7,7 @@ from access import get_access_token
 def getKite():
         token = get_access_token()
         if(token == None):
-            return
+            return None, None
         kite = KiteConnect(api_key=getConfig(
             'api_key'), access_token= token)
         kws = KiteTicker(getConfig('api_key'), token)
