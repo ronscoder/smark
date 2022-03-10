ac18f6d5fdc569f8f3e06e5bb38258214527d74b
ronscoder <rons.ubm@gmail.com>
2022-03-10 15:12:35 +0530

    option set repair

--- libs/utilities.py
+++ libs/utilities.py
@@ -1,6 +1,17 @@
 import yfinance as yf
-
+import datetime
 
 def ydownload(symbol, startdate, enddate, interval='5m'):
     return yf.download(symbol, start=startdate,
                                     end=enddate, interval=interval)
+
+def is_holiday(date: datetime.date):
+    day = date.weekday()
+    if(day in [5, 6]):
+        return True
+    from libs.configs import getConfig
+    holidays = getConfig('holidays')
+    date_string = f'{date.year}{date.month:02}{date.day:02}'
+    if(date_string in holidays):
+        return True
+    return False
--- setoptions.py
+++ setoptions.py
@@ -1,8 +1,8 @@
+import datetime
 from libs.configs import getConfig
 from libs.pubsub import get_ps_1
 from libs.init_kite import getKite
 import pdb
-
 
 def set_options(if_ce=True, if_pe=True):
     # pdb.set_trace()
@@ -14,6 +14,7 @@ def set_options(if_ce=True, if_pe=True):
     IN_CHANNEL = f'TICK_{niftybank_token}'
     tick = get_ps_1().get(IN_CHANNEL)
     if(tick is None):
+        print('tick is None')
         return
     nifty_bank_ltp = tick['last_price']
     # print(nifty_bank_ltp)
@@ -21,56 +22,54 @@ def set_options(if_ce=True, if_pe=True):
     atm = atm_ if (nifty_bank_ltp - atm_) < (atm_ +
                                              100 - nifty_bank_ltp) else atm_ + 100
     # print(atm)
-    yy = '22'
-    'next thursdays'
-    mds = getConfig('OPTION_MD')
+    today = datetime.datetime.today()
+    yy = today.strftime('%y')
+    strikes = getConfig('STRIKES')
 
-    strike_range = getConfig('STRIKE_RANGE')
-    strike_pe = (atm - strike_range['SKIP'] -
-                 strike_range['SPAN'], atm - strike_range['SKIP'])
-    strike_ce = (atm + strike_range['SKIP'], atm + strike_range['SPAN'] + strike_range['SKIP'])
+    insts = []
+    for strike in strikes:
+        msuffix = strike['suffix']
+        strike_min = strike['min']
+        strike_max = strike['max']
+        options = []
+        if(if_ce):
+            options = [f'BANKNIFTY{yy}{msuffix}{x}CE' for x in range(atm, strike_max, 100)]
+            insts.extend(options)
+        if(if_pe):
+            options = [f'BANKNIFTY{yy}{msuffix}{x}PE' for x in range(strike_min, atm, 100)]
+            insts.extend(options)
 
-    ces = []
-    if(if_ce):
-        ces = [f'BANKNIFTY{yy}{md}{strike}CE' for strike in range(
-            strike_ce[0], strike_ce[1], 100) for md in mds]
-    pes = []
-    if(if_pe):
-        pes = [f'BANKNIFTY{yy}{md}{strike}PE' for strike in range(strike_pe[0], strike_pe[1], 100) for md in mds]
-    
-    ltps = kite.ltp([f'NFO:{inst}' for inst in [*ces, *pes]])
-    # print('rate', rate)
+    ltps = kite.ltp([f'NFO:{inst}' for inst in insts])
+
     for ltp in ltps:
         print(ltp, ltps[ltp]['last_price'])
     # print(ltps)
-    print()
-    celtps = [(x, ltps[x]) for x in ltps if x[-2:] == 'CE']
-    celtps = [x for x in sorted(celtps, key=lambda x: x[1]['last_price']) if (
-        price_range[0] <= x[1]['last_price'] <= price_range[1])]
+    
     ce = None
-    if(len(celtps) > 0):
-        for ce in celtps:
-            print(ce[0], ce[1]['last_price'])
-        print(celtps[-1])
-        # symbol, token
-        ce = (celtps[-1][0][4:], celtps[-1][1]['instrument_token'])
-        # setConfig('NIFTYBANK_CE',
-        #           (celtps[-1][0][4:], celtps[-1][1]['instrument_token']))
+    if(if_ce):
+        celtps = [(x, ltps[x]) for x in ltps if x[-2:] == 'CE']
+        celtps = [x for x in sorted(celtps, key=lambda x: x[1]['last_price']) if (
+            price_range[0] <= x[1]['last_price'] <= price_range[1])]
+        if(len(celtps) > 0):
+            for ce in celtps:
+                print(ce[0], ce[1]['last_price'])
+            print(celtps[-1])
+            # symbol, token
+            ce = (celtps[-1][0][4:], celtps[-1][1]['instrument_token'])
     print()
-    peltps = [(x, ltps[x]) for x in ltps if x[-2:] == 'PE']
-    peltps = [x for x in sorted(peltps, key=lambda x: x[1]['last_price']) if (
-        price_range[0] <= x[1]['last_price'] <= price_range[1])]
     pe = None
-    if(len(peltps) > 0):
-        for pe in peltps:
-            print(pe[0], pe[1]['last_price'])
-        print(peltps[-1])
-        pe = (peltps[-1][0][4:], peltps[-1][1]['instrument_token'])
-        # setConfig('NIFTYBANK_PE',
-        #           (peltps[-1][0][4:], peltps[-1][1]['instrument_token']))
+    if(if_pe):
+        peltps = [(x, ltps[x]) for x in ltps if x[-2:] == 'PE']
+        peltps = [x for x in sorted(peltps, key=lambda x: x[1]['last_price']) if (
+            price_range[0] <= x[1]['last_price'] <= price_range[1])]
+        if(len(peltps) > 0):
+            for pe in peltps:
+                print(pe[0], pe[1]['last_price'])
+            print(peltps[-1])
+            pe = (peltps[-1][0][4:], peltps[-1][1]['instrument_token'])
     
     return ce, pe
 
 
 if(__name__ == '__main__'):
-    set_options()
+    set_options(True, True)
