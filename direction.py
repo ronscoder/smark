1126f2bdf069b04d71e101200482a59fd4dc465a
ronscoder <rons.ubm@gmail.com>
2022-03-16 10:48:35 +0530

    Add candle momentum

--- direction.py
+++ direction.py
@@ -41,10 +41,11 @@ def calculate(channel, data):
         extremas[minid] = -1
 
     print('Closes[-2]-[-1]', ltp_change_pc)
-    if((1 in extremas[-3:]) and not (-1 in extremas[-3:])):
-        direction = -1
-    elif((-1 in extremas[-3:]) and not (1 in extremas[-3:])):
-        direction = 1
+    if(abs(ltp_change_pc) > configs['CANDLE_MOMENTUM_PC']):
+        if((1 in extremas[-3:]) and not (-1 in extremas[-3:])):
+            direction = -1
+        elif((-1 in extremas[-3:]) and not (1 in extremas[-3:])):
+            direction = 1
     # if(abs(ltp_change_pc) > configs['CANDLE_MOMENTUM_PC']):
     #     #: Trending
     #     if(ltp > mashort.iloc[-1] > malong.iloc[-1]):
