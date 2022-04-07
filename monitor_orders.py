3af18a2b4d3c19b85332aab72f469604d8cf51fa
ronscoder <rons.ubm@gmail.com>
2022-04-07 15:42:42 +0530

    candle_min

--- monitor_orders.py
+++ monitor_orders.py
@@ -15,13 +15,15 @@ def action(data):
         print('There are open buy orders', len(obuyorders))
         now = datetime.datetime.now(tz=ZoneInfo('Asia/Kolkata'))
         expiry_min = getConfig('OPEN_ORDER_EXPIRY_MIN')
+        candle_min = getConfig('OHLC_MIN')
         for order in obuyorders:
             order_time = order['order_timestamp']
             timepast = now.replace(tzinfo=None) - order_time
             if(timepast > datetime.timedelta(minutes=expiry_min)):
                 print('Canceling expired open buy orders', expiry_min)
                 orderapi.cancel_open_buy_orders()
-            elif(timepast > datetime.timedelta(minutes=round(expiry_min/2-1))):
+            # elif(timepast > datetime.timedelta(minutes=round(expiry_min/2-1))):
+            elif(timepast > datetime.timedelta(minutes=candle_min)):
                 print('Changing open buy order triggers')
                 price, trigger = orderapi.get_buy_sl_prices(order['tradingsymbol'], order['exchange'])
                 print(order['tradingsymbol'], 'new price', f'{price}/{trigger}')
@@ -31,4 +33,4 @@ if(__name__ == '__main__'):
     while(True):
         action(None)
         wait_min = getConfig('OHLC_MIN')
-        time.sleep(round(wait_min*2)*60)
+        time.sleep(round(wait_min*1)*60)
