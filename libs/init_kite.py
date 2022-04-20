97673755ebf50a02e2b0cc8246011bafa933981b
ronscoder <rons.ubm@gmail.com>
2022-04-20 11:06:48 +0530

    reverted

--- libs/init_kite.py
+++ libs/init_kite.py
@@ -5,10 +5,9 @@ from access import get_access_token
 
 
 def getKite():
-        token = get_access_token()
-        if(token == None):
-            return None, None
-        kite = KiteConnect(api_key=getConfig(
-            'api_key'), access_token= token)
-        kws = KiteTicker(getConfig('api_key'), token)
-        return kite, kws
+    token = get_access_token()
+    if(token == None):
+        return None, None
+    kite = KiteConnect(api_key=getConfig('api_key'), access_token= token)
+    kws = KiteTicker(getConfig('api_key'), token)
+    return kite, kws
